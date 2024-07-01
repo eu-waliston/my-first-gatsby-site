cb4ca65293a4bf72252c318fb412d14004289465
Waliston <wesantosdev@gmail.com>
2024-07-01 20:20:49 -0300

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const adapter = require("gatsby-adapter-netlify")
+
+
+
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
+  adapter: adapter({
+    excludeDatastoreFromEngineFunction: false,
+  }),
 }
